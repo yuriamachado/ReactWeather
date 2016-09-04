2b38250085b4fd1a5e49c0264304685eadfa7e30
Yuri Alfonso machado <Yuri Alfonso machado>
2016-09-04 19:13:47 -0300

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ var express = require('express');
 
 //Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+// req(requistion) = requisiition the in file
+// res(responsive) = responsive what send back
+// next = when the piece of middleware is done (function)
+app.use(function(req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-  console.log('Exporess server is up on port 3000');
+
+app.listen(PORT, function(){
+  console.log('Exporess server is up on port ' + PORT);
 });
