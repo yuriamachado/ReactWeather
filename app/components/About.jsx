90aecda930b5a7a6c3129eb815691352365bf4ac
Yuri Alfonso machado <Yuri Alfonso machado>
2016-09-04 19:33:37 -0300

    Rename about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,16 +1,11 @@
 var React = require('react');
 
-// var About = React.createClass({
-//     render: function() {
-//       return(
-//         <h3>About Component</h3>
-//       )
-//     }
-// });
-
 var About = (props) => {
   return(
-    <h3>About Component</h3>
+    <div>
+      <h3>About</h3>
+      <p>Welcome to about page!</p>
+    </div>
   )
 };
 
